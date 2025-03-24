4d8bf342993f53f99659c2a1a41bebad1b1f1baa
Aaron Chan <aarchan108@gmail.com>
2025-03-24 10:01:12 -0400

    Fix more possible delay drift by updating LEDControl with FreeRTOS proper calls. "Optimize" checking specific cases for setting led color

--- Core V2.4.1/SignInReader/LEDControl.ino
+++ Core V2.4.1/SignInReader/LEDControl.ino
@@ -1,35 +1,41 @@
 void LEDControl(void *pvParameters){
-  while(1){
-    delay(20);
-    //Go through and check states to see what we should be doing;
+  TickType_t xLastWakeTime;
+  const TickType_t xFrequency = 20 / portTICK_PERIOD_MS;
+  
+  xLastWakeTime = xTaskGetTickCount();
+  
+  while (1) {
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+    
+    // Go through and check states to see what we should be doing
     byte red = 0;
     byte green = 0;
     byte blue = 0;
-    if(!Ready){
-      red = 255;
-      green = 255;
-      blue = 0;
-    }
-    if(InSystem){
-      green = 255;
-      red = 0;
-      blue = 0;
-    }
-    if(NotInSystem || InvalidCard){
-      red = 255;
-      green = 0;
-      blue = 0;
-    }
-    if(NetworkCheck){
-      red = 255;
-      green = 255;
-      blue = 255;
-    }
-    if(!digitalRead(Button)){
+
+    // TODO: Validate this new ordering. Following logic based on previous conditions
+    // Prob a micro op tho
+    if (!digitalRead(Button)) {
       red = 0;
       green = 0;
       blue = 255;
+    } else if (NetworkCheck) {
+      red = 255;
+      green = 255;
+      blue = 255;
+    } else if (InSystem) {
+      green = 255;
+      red = 0;
+      blue = 0;
+    } else if (NotInSystem || InvalidCard) {
+      red = 255;
+      green = 0;
+      blue = 0;
+    } else if (!Ready) {
+      red = 255;
+      green = 255;
+      blue = 0;
     }
+
     LEDColor(red, green, blue);
   }
 }
