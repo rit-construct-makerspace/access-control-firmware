c02935ccd938588f5441f4a1cf869e22f75b4b19
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2025-06-25 12:46:52 -0400

    Made some types

---
+++ Core V2.4.1/main/common/types.hpp
@@ -0,0 +1,81 @@
+#pragma once
+#include <array>
+#include <cstdint>
+#include <string>
+
+enum class CardTagType {
+    SEVEN,
+    FOUR,
+};
+
+struct CardTagID {
+    CardTagType type;
+    std::array<uint8_t, 7> value;
+};
+
+enum IOState {
+    IDLE,
+    UNLOCKED,
+    ALWAYS_ON,
+    LOCKOUT,
+    NEXT_CARD,
+    STARTUP,
+    WELCOMING,
+    ALWAYS_ON_WAITING,
+    LOCKOUT_WAITING,
+    IDLE_WAITING,
+    AWAIT_AUTH,
+    DENIED,
+    FAULT,
+    RESTART,
+};
+
+enum class LogMessageType {
+    NORMAL,
+    DEBUG,
+    ERROR,
+};
+
+struct LogMessage {
+    LogMessageType type;
+    std::string message;
+};
+
+enum class IOEventType {
+    BUTTON_PRESSED,
+    CARD_DETECTED,
+    CARD_REMOVED,
+    NETWORK_COMMAND,
+};
+
+struct ButtonEvent {
+
+};
+
+struct CardDetectedEvent {
+    CardTagID card_tag_id;
+};
+
+struct CardRemovedEvent {
+
+};
+
+enum class NetworkCommandEventType {
+    IDENTIFY,
+    COMMAND_STATE,
+};
+
+struct NetworkCommandEvent {
+    NetworkCommandEventType type;
+    IOState commanded_state; // Only valid if type is COMMAND_STATE
+};
+
+struct IOEvent {
+    IOEventType type;
+    union {
+        ButtonEvent button;
+        CardDetectedEvent card_detected;
+        CardRemovedEvent card_removed;
+        NetworkCommandEvent network_command;
+    };
+};
