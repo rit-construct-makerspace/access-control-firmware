bb803cdea7b993a05bd457f3d8026298203188c5
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2025-06-30 09:29:17 -0400

    CardReader structure

---
+++ Core-v2.4.1/main/io/CardReader.cpp
@@ -0,0 +1,23 @@
+#include "CardReader.hpp"
+
+#include "mfrc630.h"
+
+void mfrc630_SPI_transfer(const uint8_t* tx, uint8_t* rx, uint16_t len) {
+
+}
+
+void mfrc630_SPI_select() {
+
+}
+
+void mfrc630_SPI_unselect() {
+    
+}
+
+void card_reader_thread_fn(void *) {
+
+}
+
+void CardReader::init() {
+
+}
---
+++ Core-v2.4.1/main/io/CardReader.hpp
@@ -0,0 +1,3 @@
+namespace CardReader {
+    void init();
+}
