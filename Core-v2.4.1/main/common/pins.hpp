52e9239b5b94af0e884d404f5be0d43c34380a61
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2025-06-30 10:06:11 -0400

    Intialized spi bus and device

--- Core-v2.4.1/main/common/pins.hpp
+++ Core-v2.4.1/main/common/pins.hpp
@@ -2,3 +2,7 @@
 
 constexpr gpio_num_t BUTTON_PIN = GPIO_NUM_0;
 #define LED_PIN 1
+#define CS_NFC 6
+#define SPI_MOSI 8
+#define SPI_CLK 10
+#define SPI_MISO 11
--- Core-v2.4.1/main/io/CardReader.hpp
+++ Core-v2.4.1/main/io/CardReader.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 namespace CardReader {
     void init();
 }
